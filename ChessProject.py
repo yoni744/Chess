d672ed3c4f8986f90f688961ea3dce176c857ec0
yoni744 <yoniparnes@gmail.com>
2023-09-05 14:22:27 +0300

    s

--- ChessProject.py
+++ ChessProject.py
@@ -3,9 +3,13 @@ import pygame as pg
 import pygame
 import chess
 import threading
+import time
 
 BLACK = pg.Color('grey')
 WHITE = pg.Color('white')
+
+board = chess.Board()
+print(board.legal_moves)
 
 #Setup(loading images, making lists.):
 white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
@@ -42,10 +46,13 @@ turn_step = 0
 selection = 100
 valid_moves = []
 
+
 def DrawBoard():
+    global colors
     colors = itertools.cycle((WHITE, BLACK))
     tile_size = 60
     width, height = 8*tile_size, 8*tile_size
+    global background
     background = pg.Surface((width, height))
 
     for y in range(0, height, tile_size):
@@ -55,7 +62,7 @@ def DrawBoard():
         next(colors)
 
     screen.fill((20, 70, 90))
-    screen.blit(background, (63, 60))
+    screen.blit(background, (60, 60))
 
     pg.display.flip()
     clock.tick(30)
@@ -63,11 +70,103 @@ def DrawBoard():
 
 
 def DrawPieces():
+        for i in range(len(black_pieces)):
+            if black_pieces[i] == "pawn":
+                for i in range(9):
+                    if i == 0:
+                        i = 1
+                        screen.blit(black_pawn, (i * 60, 120,))
+                        i = 0
+                    
+                    else:
+                        screen.blit(black_pawn, (i * 60, 120,))
+
+            else:
+                if black_pieces[i] == "rook":
+                    screen.blit(black_rook, (480, 60))
+                    screen.blit(black_rook, (60, 60))
+
+                if black_pieces[i] == "knight":
+                    screen.blit(black_knight, (120, 60))
+                    screen.blit(black_knight, (420, 60))
+
+                if black_pieces[i] == "bishop":
+                    screen.blit(black_bishop, (180, 60))
+                    screen.blit(black_bishop, (360, 60))
+
+                if black_pieces[i] == "king":
+                    screen.blit(black_king, (240, 60))
+
+                if black_pieces[i] == "queen":
+                    screen.blit(black_queen, (302, 60))
+
+
         for i in range(len(white_pieces)):
             if white_pieces[i] == "pawn":
-                for i in range(8):
-                    screen.blit(white_pawn, (i * 60, 1))
+                for i in range(9):
+                    if i == 0:
+                        i = 1
+                        screen.blit(white_pawn, (i * 60, 420,))
+                        i = 0
+                    
+                    else:
+                        screen.blit(white_pawn, (i * 60, 420,))
+
+            else:
+                if white_pieces[i] == "rook":
+                    screen.blit(white_rook, (480, 480))
+                    screen.blit(white_rook, (60, 480))
+
+                if white_pieces[i] == "rook":
+                    screen.blit(white_rook, (480, 480))
+                    screen.blit(white_rook, (60, 480))
+
+                if white_pieces[i] == "knight":
+                    screen.blit(white_knight, (120, 480))
+                    screen.blit(white_knight, (420, 480))
+
+                if white_pieces[i] == "bishop":
+                    screen.blit(white_bishop, (180, 480))
+                    screen.blit(white_bishop, (360, 480))
+
+                if white_pieces[i] == "king":
+                    screen.blit(white_king, (240, 480))
+
+                if white_pieces[i] == "queen":
+                    screen.blit(white_queen, (300, 480))
             
+
+def CheckClick():
+    left = pg.mouse.get_pressed()
+    pg.mouse.get_rel()
+    pos = 0
+    x = 0
+    y = 0
+    global redSquare
+    global colour
+    if left[0]:
+        print(redSquare)
+        if redSquare == -1:
+            pos = pg.mouse.get_pos()
+            x = pos[0] // 60
+            y = pos[1] // 60
+            colour = (255, 0, 0)
+            pg.draw.rect(screen, colour, pg.Rect(x * 60, y * 60, 60, 60))
+            redSquare = x, y
+            DrawPieces()
+        
+        else:
+            DrawBoard()
+            redSquare = -1
+            pos = pg.mouse.get_pos()
+            x = pos[0] // 60
+            y = pos[1] // 60
+            #pg.draw.rect(screen, next(colors), pg.Rect(redSquare[0] * 60, redSquare[1] * 60, 60, 60))
+            pg.draw.rect(screen, (255, 0, 0), pg.Rect(x * 60, y * 60, 60, 60))
+            DrawPieces()
+
+            
+
 
 def main():
     pg.init()
@@ -78,11 +177,17 @@ def main():
     DrawBoard()
     DrawPieces()
     running = True
+    global redSquare
+    redSquare = -1
     
     while running:
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 running = False
+            
+            else:
+                CheckClick()
+                
 
         clock.tick(60)
         pg.display.flip()
